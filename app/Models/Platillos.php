2f1613dd830a518c74b95790e86e6294a5cfa47d
Brisa257 <beriricalday@gmail.com>
2023-11-28 22:39:06 -0600

    Update Platillos.php

--- app/Models/Platillos.php
+++ app/Models/Platillos.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Categorias;
+use App\Models\OrdenDetaills;
 use Illuminate\Database\Eloquent\Model;
 
 class Platillos extends Model
@@ -14,6 +15,11 @@ class Platillos extends Model
 
     public function categoria()
     {
-        return $this->belongsTo(Categorias::class);
+        return $this->belongsTo(Categorias::class, 'categoria_id');
+    }
+
+    public function ordenDetallis()
+    {
+        return $this->hasMany(OrdenDetaills::class, 'order_id');
     }
 }
