b83de7406c6a6a9d10d704503e03f284f49e3a1a
Brisa257 <beriricalday@gmail.com>
2023-12-01 19:53:46 -0600

    Update PlatillosController.php

--- app/Http/Controllers/PlatillosController.php
+++ app/Http/Controllers/PlatillosController.php
@@ -6,6 +6,8 @@ use App\Models\Platillos;
 use Intervention\Image\Facades\Image;
 use Illuminate\Http\Request;
 use App\Models\Categorias;
+use Dompdf\Dompdf;
+use Illuminate\Support\Facades\Storage;
 
 class PlatillosController extends Controller
 {
@@ -15,6 +17,42 @@ class PlatillosController extends Controller
 
         return view('platillos.index', compact('platillos'));
     }
+
+    public function generatePDF()
+    {   
+        $dompdf = new Dompdf();
+    
+        $platillos = Platillos::all();
+        // Convertir imágenes a Base64 y añadirlas al array de platillos
+        foreach ($platillos as $platillo) {
+            if ($platillo->imagen) {
+                $imagePath = public_path('carpeta_imagenes/' . $platillo->imagen);
+                $imageBase64 = $this->getImageBase64($imagePath);
+                $platillo->imagen = $imageBase64;
+            }
+        }
+
+        $html = view('platillos.pdf', compact('platillos'))->render();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        
+        $pdfContent = $dompdf->output();
+        Storage::disk('public')->put('PDFs/Platillos.pdf', $pdfContent);
+
+        // Devolver el contenido del PDF
+        return response($pdfContent)
+        ->header('Content-Type', 'application/pdf')
+        ->header('Content-Disposition', 'inline; filename="platillos.pdf"');
+    }
+
+    function getImageBase64($imagePath) {
+        $imageData = file_get_contents($imagePath);
+        $base64 = 'data:image/' . pathinfo($imagePath, PATHINFO_EXTENSION) . ';base64,' . base64_encode($imageData);
+        return $base64;
+    }
+
+
 
     public function create()
     {
